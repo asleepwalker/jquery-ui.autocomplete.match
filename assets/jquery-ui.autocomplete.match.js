8dc8e0d3075f6af0a96f032923a57b30f18da6dc
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-02-10 13:44:54 +0200

    Add UMD

--- assets/jquery-ui.autocomplete.match.js
+++ assets/jquery-ui.autocomplete.match.js
@@ -1,24 +1,46 @@
 /*
-*	jQueryUI.Autocomplete.Match, v1.0.1
-*	(c) 2014–2015 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
+*	jQueryUI.Autocomplete.Match, v1.0.2
+*	(c) 2014–2017 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
 *	https://github.com/asleepwalker/jquery-ui.autocomplete.match.js
 */
 
-(function($) {
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		module.exports = function (root, jQuery) {
+			if (jQuery === undefined) {
+				if (typeof window !== 'undefined') {
+					jQuery = require('jquery');
+				} else {
+					jQuery = require('jquery')(root);
+				}
+			}
+			factory(jQuery);
+			return jQuery;
+		};
+	} else {
+		factory(jQuery);
+	}
+}(function ($) {
 
 	$.extend($.ui.autocomplete.prototype, {
 		highlight: false,
-		_renderItem: function(ul, item) {
-			var term = this.element.val(),
-				matcher = new RegExp($.ui.autocomplete.escapeRegex(term), 'gi'),
-				label = item.label,
-				matchClass = typeof this.options.highlight == 'string' ? this.options.highlight : 'ui-autocomplete-match';
+		_renderItem: function (ul, item) {
+			var term = this.element.val();
+			var matcher = new RegExp($.ui.autocomplete.escapeRegex(term), 'gi');
+			var label = item.label;
+			var matchClass = typeof this.options.highlight == 'string' ?
+				this.options.highlight
+				: 'ui-autocomplete-match';
 
 			return $('<li>')
 				.data('item.autocomplete', item)
-				.html(this.options.highlight ? label.replace(matcher, '<span class="' + matchClass + '">$&</span>') : label)
+				.html(this.options.highlight ?
+					label.replace(matcher, '<span class="' + matchClass + '">$&</span>')
+					: label)
 				.appendTo(ul);
 		}
 	});
 
-})(jQuery);
+}));
